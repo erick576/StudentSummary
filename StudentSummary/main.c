83082ab20231ba99adf9fe938e96dc9fedf843e2
Eric Kwok <46385457+Kwok-Eric-216397150@users.noreply.github.com>
2019-10-14 00:40:08 -0400

    Input
    First Change

--- StudentSummary/main.c
+++ StudentSummary/main.c
@@ -3,9 +3,19 @@
 
 int main()
 {
- FILE * fpointer = fopen("employees.txt", "w");
+ FILE * fpointer = fopen("studentsummary.txt", "w");
 
-fprintf(fpointer, "Jim, Salesman\nPam, Receptionist\nOscar, Accounting");
+printf("Hi! What is your name?");
+char name[100];
+scanf("%[^\n]s", &name);
+fprintf(fpointer, "Name: %s\n", name);
+
+
+printf("What College/University Do You Attend?");
+char school[50];
+char type[30];
+scanf("%s%s", &school, &type);
+fprintf(fpointer, "School: %s %s\n", school, type);
 
 fclose(fpointer);
 
