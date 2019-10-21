cebcf493335bf1e7f3090ef87adeebb798b627ea
Eric Kwok <46385457+Kwok-Eric-216397150@users.noreply.github.com>
2019-10-21 13:20:23 -0400

    Course Input

--- StudentSummary/main.c
+++ StudentSummary/main.c
@@ -17,6 +17,19 @@ char type[30];
 scanf("%s%s", &school, &type);
 fprintf(fpointer, "School: %s %s\n", school, type);
 
+printf("How many courses are you currently taking?");
+int num;
+scanf("%i", &num);
+fprintf(fpointer, "Number Of Courses: %i\n", num);
+int i = 1;
+while(i <= num) {
+printf("What was the %ith course?", i);
+char course[10];
+scanf(" %s", course);
+fprintf(fpointer, "Course %i: %s\n" ,i, course);
+i ++;
+}
+
 fclose(fpointer);
 
     return 0;
