31ac7f605d6af6d3a1334f6bf05e0903b13663fd
Eric Kwok <46385457+Kwok-Eric-216397150@users.noreply.github.com>
2020-04-02 23:07:00 -0400

    Format Prompt

--- StudentSummary/main.c
+++ StudentSummary/main.c
@@ -1,36 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
- FILE * fpointer = fopen("studentsummary.txt", "w");
+    FILE * fpointer = fopen("studentsummary.txt", "w");
 
-printf("Hi! What is your name?");
-char name[100];
-scanf("%[^\n]s", &name);
-fprintf(fpointer, "Name: %s\n", name);
+    printf("Hi! What is your name?\n");
+
+    char name[100];
+    scanf("%[^\n]s", name);
+    fprintf(fpointer, "Name: %s\n", name);
 
 
-printf("What College/University Do You Attend?");
-char school[50];
-char type[30];
-scanf("%s%s", &school, &type);
-fprintf(fpointer, "School: %s %s\n", school, type);
+    printf("What College/University Do You Attend?\n");
+    char school[50], type[50];
+    scanf("%s %s", school, type);
+    fprintf(fpointer, "School: %s\n", school);
 
-printf("How many courses are you currently taking?");
-int num;
-scanf("%i", &num);
-fprintf(fpointer, "Number Of Courses: %i\n", num);
-int i = 1;
-while(i <= num) {
-printf("What was the %ith course?", i);
-char course[10];
-scanf(" %s", course);
-fprintf(fpointer, "Course %i: %s\n" ,i, course);
-i ++;
-}
+    printf("How many courses are you currently taking?\n");
+    int num;
+    scanf("%d", &num);
+    fprintf(fpointer, "Number Of Courses: %i\n", num);
+    int i = 1;
+    float gpa;
+    while(i <= num)
+    {
+        printf("Course %d Name?\n", i);
+        char course[10];
+        scanf("%s", course);
 
-fclose(fpointer);
+        printf("What is Your Grade in %s?\n", course);
+
+        float cg;
+        scanf("%f",  &cg);
+        gpa = gpa + cg;
+        fprintf(fpointer, "Course %d: %s\n", i, course);
+        i ++;
+    }
+
+    float cgpa = gpa / num;
+    fprintf(fpointer, "GPA: %f", cgpa);
+    fclose(fpointer);
 
     return 0;
 }
